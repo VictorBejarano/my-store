cf36d516c096688d3f8a73f827175ae4cdc155e6
Victor Bejarano <alejandrobjrn4@gmail.com>
2022-03-20 00:07:48 -0500

    Custom components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
 import { Component } from '@angular/core';
+import { Product } from './models/product.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   imgParent = 'my-store';
+  products: Product[] = [
+    {
+      id: '1',
+      name: 'Product 1',
+      image: 'https://picsum.photos/200/300',
+      price: 100,
+    },
+    {
+      id: '2',
+      name: 'Product 2',
+      image: 'https://picsum.photos/200/300',
+      price: 200,
+    },
+    {
+      id: '3',
+      name: 'Product 3',
+      image: 'https://picsum.photos/200/300',
+      price: 300,
+    },
+    {
+      id: '4',
+      name: 'Product 4',
+      image: 'https://picsum.photos/200/300',
+      price: 400,
+    },
+  ];
 }
