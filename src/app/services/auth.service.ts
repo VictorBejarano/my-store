6324e09befb634e6663258c6af15a73ffc326fb0
Victor Bejarano <alejandrobjrn4@gmail.com>
2022-03-21 18:04:10 -0500

    Enviar token con un interceptor

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,41 +1,42 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { switchMap } from 'rxjs/operators';
+import { switchMap, tap } from 'rxjs/operators';
 
 import { environment } from './../../environments/environment';
 import { Auth } from './../models/auth.model';
 import { User } from './../models/user.model';
+import { TokenService } from './token.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
-
   private apiUrl = `${environment.API_URL}/api/auth`;
 
-  constructor(
-    private http: HttpClient
-  ) { }
+  constructor(private http: HttpClient, private tokenService: TokenService) {}
 
   login(email: string, password: string) {
-    return this.http.post<Auth>(`${this.apiUrl}/login`, {email, password});
+    return this.http
+      .post<Auth>(`${this.apiUrl}/login`, { email, password })
+      .pipe(
+        tap((response) => this.tokenService.saveToker(response.access_token))
+      );
   }
 
-  getProfile(token: string) {
+  getProfile() {
     // const headers = new HttpHeaders();
     // headers.set('Authorization',  `Bearer ${token}`);
     return this.http.get<User>(`${this.apiUrl}/profile`, {
-      headers: {
-        Authorization: `Bearer ${token}`,
-        // 'Content-type': 'application/json'
-      }
+      // headers: {
+      //   Authorization: `Bearer ${token}`,
+      //   // 'Content-type': 'application/json'
+      // },
     });
   }
 
   loginAndGet(email: string, password: string) {
-    return this.login(email, password)
-    .pipe(
-      switchMap(rta => this.getProfile(rta.access_token)),
-    )
+    return this.login(email, password).pipe(
+      switchMap(() => this.getProfile())
+    );
   }
 }
