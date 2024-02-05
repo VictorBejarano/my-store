1cabd90a32979f2af23abe6352b217383d31ee04
Victor Bejarano <alejandrobjrn4@gmail.com>
2024-02-05 15:15:58 -0500

    10. Validadores y validar expresiones regulares

--- src/app/admin/components/basic-form/basic-form.component.ts
+++ src/app/admin/components/basic-form/basic-form.component.ts
@@ -38,15 +38,22 @@ export class BasicFormComponent implements OnInit {
 
   private buidForm() {
     this.form = this.formBuilder.group({
-      name: ['', [Validators.required, Validators.maxLength(10)]],
-      email: [''],
+      name: [
+        '',
+        [
+          Validators.required,
+          Validators.maxLength(10),
+          Validators.pattern(/^([Aa-zA-ZáéíóúÁÉÍÓÚÑñ]{2,}\s?){2,4}$/),
+        ],
+      ],
+      email: ['', [Validators.required, Validators.email]],
       phone: ['', Validators.required],
       color: ['#000000'],
       date: [''],
-      age: [12],
+      age: [12, [Validators.required, Validators.min(18), Validators.max(100)]],
       category: ['category-2'],
       tag: [''],
-      agree: [false],
+      agree: [false, [Validators.requiredTrue]],
       gender: [''],
       zone: [''],
     });
