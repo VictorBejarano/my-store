6789d19939d740cb94b9af2c56088d289fd688d7
Victor Bejarano <alejandrobjrn4@gmail.com>
2024-02-06 18:51:16 -0500

    14. Validaciones grupales

--- src/app/utils/validators.ts
+++ src/app/utils/validators.ts
@@ -17,6 +17,19 @@ export class MyValidators {
     }
     return null;
   }
+
+  static matchPasswords(control: AbstractControl) {
+    const password = control.get('password').value;
+    const confirmPassword = control.get('confirmPassword').value;
+
+    if (password === confirmPassword) {
+      return null;
+    }
+
+    return {
+      match_password: true,
+    };
+  }
 }
 
 function containsNumber(value: string) {
