d6ccedf548ad801620d0dd3220141d3aae2e33e1
Victor Bejarano <alejandrobjrn4@gmail.com>
2024-02-16 18:19:17 -0500

    31. Pruebas inválidas del parámetro

--- src/app/products/components/product-detail/product-detail.component.spec.ts
+++ src/app/products/components/product-detail/product-detail.component.spec.ts
@@ -1,11 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
-import {
-  ActivatedRouteStub,
-  getText,
-  mockObservable,
-} from '../../../../testing';
+import { ActivatedRouteStub, getText, mockObservable } from '../../../../testing';
 import { ProductsService } from '../../../services/product.service';
 import { generateOneProduct } from '../../../models/product.mock';
 
@@ -20,28 +16,25 @@ fdescribe('ProductDetailComponent', () => {
 
   beforeEach(async () => {
     const routeStub = new ActivatedRouteStub();
-    const productServiceSpy = jasmine.createSpyObj('ProductsService', [
-      'getOne',
-    ]);
+    const productServiceSpy = jasmine.createSpyObj('ProductsService', ['getOne']);
     const locationSpy = jasmine.createSpyObj('Location', ['back']);
 
     await TestBed.configureTestingModule({
-      declarations: [ProductDetailComponent],
+      declarations: [ ProductDetailComponent ],
       providers: [
-        { provide: ActivatedRoute, useValue: routeStub },
-        { provide: ProductsService, useValue: productServiceSpy },
-        { provide: Location, useValue: locationSpy },
-      ],
-    }).compileComponents();
+        { provide:  ActivatedRoute, useValue: routeStub },
+        { provide:  ProductsService, useValue: productServiceSpy },
+        { provide:  Location, useValue: locationSpy },
+      ]
+    })
+    .compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ProductDetailComponent);
     component = fixture.componentInstance;
     route = TestBed.inject(ActivatedRoute) as unknown as ActivatedRouteStub;
-    productsService = TestBed.inject(
-      ProductsService
-    ) as jasmine.SpyObj<ProductsService>;
+    productsService = TestBed.inject(ProductsService) as jasmine.SpyObj<ProductsService>;
     location = TestBed.inject(Location) as jasmine.SpyObj<Location>;
   });
 
@@ -52,7 +45,7 @@ fdescribe('ProductDetailComponent', () => {
     const productMock = {
       ...generateOneProduct(),
       id: productId,
-    };
+    }
 
     productsService.getOne.and.returnValue(mockObservable(productMock));
 
@@ -67,15 +60,23 @@ fdescribe('ProductDetailComponent', () => {
     const productMock = {
       ...generateOneProduct(),
       id: productId,
-    };
+    }
 
     productsService.getOne.and.returnValue(mockObservable(productMock));
+
 
     fixture.detectChanges(); //ngOnInit
     const titleText = getText(fixture, 'title');
     const priceText = getText(fixture, 'price');
     expect(titleText).toContain(productMock.title);
-    expect(priceText).toContain(productMock.price.toString());
+    expect(priceText).toContain(productMock.price);
     expect(productsService.getOne).toHaveBeenCalledWith(productId);
+  });
+
+  it('should go to back without id params', () => {
+    route.setParamMap({});
+    location.back.and.callThrough(); // mocking
+    fixture.detectChanges(); //ngOnInit
+    expect(location.back).toHaveBeenCalled();
   });
 });
